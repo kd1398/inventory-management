ee08f3ae1761b86b400beb5e53cfa046fc16c1f2
Chintal Raval <chintaldraval@gmail.com>
2023-08-21 17:49:52 -0400

    WIP JWT

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { authenticateToken } from "./middleware/authentication/jwt.js";
 
 // const uri = `mongodb+srv://user1000:test123@cluster0.qdpbtz9.mongodb.net/?retryWrites=true&w=majority` || `mongodb://127.0.0.1:27017`;
 const uri = `mongodb://0.0.0.0:27017`;
@@ -28,6 +29,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/user", userRoutes);
+app.use("/test/jwt", authenticateToken, testRoutes);
 
 // port
 const port = process.env.PORT || 8080;
@@ -38,6 +40,19 @@ const server = async () => {
     console.log(`Server is running on port http://localhost:${port}`)
   );
 };
+
+try {
+  const jwtSecretKey = process.env.JWTKEY.toString();
+  if (jwtSecretKey === "" || jwtSecretKey.split(" ")[1] != undefined) {
+    console.log("Could not find JWT Secret Key");
+    throw new Error("");
+  } else {
+    jwtSecretKey = process.env.JWTKEY.toString();
+  }
+} catch (error) {
+  console.log("Error reading JWT key");
+  // TODO: Server closing Functionality
+}
 
 // If server not run
 process.on("unhandledRejection", (err) => {
